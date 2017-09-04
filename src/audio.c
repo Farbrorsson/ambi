156a2657b30ecfe15e490db06ba122cfa69062cb
Viktor Sannum <sannum.viktor@gmail.com>
2017-09-04 14:58:25 +0200

    Simplifying audio.c

--- src/audio.c
+++ src/audio.c
@@ -40,21 +40,19 @@ void prepareInterface(Audio *a) {
 	}
 }
 
-static char *device = "plughw:1,0";                        /* playback device */
-snd_output_t *output = NULL;
-unsigned char buffer[16*1024];        
+//snd_output_t *output = NULL;
+//unsigned char buffer[16*1024];        
 
 void start(Audio *a) {
 	int nChannels = 2;
 	int f = 44100;
 	int i;
 	int err;
-	unsigned char buf[256];
 	
 	 snd_pcm_sframes_t frames;
 	
 	for (i = 0; i < 16*1024; i++) {
-		buffer[i] = (i % 256) > 128 ? 0x66 : 0x99;
+		a->buffer[i] = (i % 256) > 128 ? 0x7a : 0x86;
 	}
 
 	audio_open(a);
@@ -63,7 +61,7 @@ void start(Audio *a) {
 	prepareInterface(a);
 	
 	for (i = 0; i < 16; i++) {
-        frames = snd_pcm_writei(a->pcm, buffer, sizeof(buffer));
+        frames = snd_pcm_writei(a->pcm, a->buffer, sizeof(buffer));
         if (frames < 0) {
         	printf("snd_pcm_recover\n");
             frames = snd_pcm_recover(a->pcm, frames, 0);
--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "audio.h"
 
+static char* device = "plughw:1,0";
+
 int main(int argc, char** argv) {
-	unsigned char buf[256];
+	unsigned char buf[16*1024];
 	Audio a;
-	a.device = argv[1];
+	a.device = device;
 	a.buffer = buf;
-	a.bufferSize = 256;
+	a.bufferSize = 16*1024;
 	start(&a);
 	return 0;
 }
